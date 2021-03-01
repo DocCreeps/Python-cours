c170373c61cea3bd3e9d18a41d205582bfd6c673
DocCreeps <dorian.vericel0546@gmail.com>
2021-03-01 17:10:45 +0100

    Update tp1.py

--- tp1.py
+++ tp1.py
@@ -5,6 +5,7 @@ Created on Mon Mar  1 14:47:44 2021
 @author: dorian
 """
 from math import sin
+from math import cos
 
 #sin x= sigma((-1)^n * X^(2n +1)) / (2n+1)
 
@@ -12,17 +13,23 @@ n = float(input('entrez une valeur pour n : \n '))
 print(n)
 i = 1
 N = i
-X = 0
+SinX = 0
+CosX = 0
 while i <= (n):
    N= N*i 
    i += 1
-   x= i - ((i^2)/3)
-   X = x + X
+   sinx= i - ((i^3)/3)
+   cosx = 1-((i^2)/2)
+   SinX = sinx + SinX
+   CosX = cosx + CosX
+   
 
 print("N = ", N)
-print("X = ", X)
-  
-print("sin x =", sin(X))
+print("SINX = ", SinX)
+print("COSX = ", CosX)
+
+print("sin x =", sin(SinX))
+print("cos x = ", cos(CosX))
 
 #pi = 
 
